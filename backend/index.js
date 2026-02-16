afac28180b4b03a54d1af54fa93d0fb4decdf030
Spcoder99 <ITMET24S0078@itmuniversity1.ac.in>
2026-02-16 17:14:51 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -91,7 +91,13 @@ async function startServer() {
     connectDB();
 
     // cors -> what is it use here -> what is the origin -> * , Explain the use of cors
-    app.use(cors({ origin: "*" }));
+  app.use(cors({
+  origin: [
+    process.env.FRONTEND_URL || "http://localhost:5173"
+  ],
+  credentials: true
+}));
+
 
    app.use( mainRouter);
 
