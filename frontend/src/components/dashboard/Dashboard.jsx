09bbe39ce9346bb59fda1874019a84fa24d6e1c6
Shubhantak Pandey <ITMET24S0078@itmuniversity1.ac.in>
2026-02-17 17:40:39 +0530

    Refactor loading state handling in Dashboard component
    Updated loading state management to use setTimeout for better UX.

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -83,7 +83,8 @@ const Dashboard = () => {
         toast.error(err?.response?.data?.error || "Failed to fetch starred repos");
         console.error("Failed to fetch starred repos", err);
       } finally {
-        setLoading(false);
+        // setLoading(false);
+        setTimeout(() => setLoading(false), 500);
       }
     };
 
@@ -107,19 +108,22 @@ const Dashboard = () => {
         toast.error(error?.message || "Failed to fetch repositories");
       } finally {
         // setLoading(false);
-        setTimeout(() => setLoading(false), 600);
+        setTimeout(() => setLoading(false), 500);
         
       }
     };
 
     const fetchSuggestedRepositories = async () => {
       try {
+        setLoading(true);
         const response = await fetch(`${import.meta.env.VITE_API_URL}/repo/all`);
         const data = await response.json();
         setSuggestedRepositories(Array.isArray(data?.repositories) ? data?.repositories : []);
       } catch (error) {
         console.error("Error While fetching repositories:", error);
         toast.error(error?.message);
+      } finally {
+        setTimeout(() => setLoading(false), 500);
       }
     };
     fetchRepositories();
