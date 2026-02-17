4721aa102adf5c065648500d2a1f55dc378031e7
Shubhantak Pandey <ITMET24S0078@itmuniversity1.ac.in>
2026-02-17 12:03:29 +0530

    Enable loading state during repository fetch

--- frontend/src/components/MyRepos/MyRepo.jsx
+++ frontend/src/components/MyRepos/MyRepo.jsx
@@ -128,7 +128,7 @@ const MyRepo = () => {
     const fetchRepositories = async () => {
       try {
         // अगर profileId है तो वही, नहीं तो मेरी id
-        // setLoading(true);
+        setLoading(true);
         const idToUse = profileId ? profileId : userId;
 
         const response = await fetch(
@@ -143,7 +143,7 @@ const MyRepo = () => {
         console.error("Error while fetching repositories:", error);
         toast.error(error?.message||"Failed to fetch repositories");
       } finally {
-        // setLoading(false);
+        setLoading(false);
         // setTimeout(() => setLoading(false), 600);
         
       }
